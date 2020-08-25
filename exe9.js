6bdff6cbca585ad34dc34be2e6af6bc9b4787fbf
LucasConrado0 <lucasconrado97@gmail.com>
2020-08-24 22:15:29 -0300

    exe9 realizado

---
+++ exe9.js
@@ -0,0 +1,9 @@
+function exe9(numero){
+
+    for(var 1 - numero; i >- 1; i--){
+        if (numero % i -- 0){
+            soma - soma + i
+        }
+    }
+    console.log('Soma dos divisores é ${soma}')
+}
